28dff5a877c615f2f81cb3402cd83529735ef9d0
sam <sam>
2024-07-02 16:08:01 -0600

    added beginnings of goShark project

---
+++ goShark/goShark.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	snaplen = int32(1600)
+	promisc = false
+	timeout = pcap.BlockForever
+	filter  = "tcp and port 80"
+)
+
+func devInfo() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, device := range devices {
+		fmt.Println(device.Name)
+		for _, address := range device.Addresses {
+			fmt.Printf("\tIP:%s\n", address.IP)
+		}
+	}
+}
+func main() {
+	devInfo()
+	var intf string
+	fmt.Print("Interface to use: ")
+	fmt.Scanf("%s", &intf)
+
+	handle, err := pcap.OpenLive(intf, snaplen, promisc, timeout)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer handle.Close()
+
+	if err := handle.SetBPFFilter(filter); err != nil {
+		log.Panicln(err)
+	}
+
+	source := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range source.Packets() {
+		fmt.Println(packet)
+	}
+}
