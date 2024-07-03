356ee67407ab536bac143eea2ec330f058da8dcb
sam <sam>
2024-07-03 09:23:52 -0600

    reorganized codebase and added formatPrinting

---
+++ 02-formatPrinting/formatPrinting.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name, age = "Kim", 22
+	fmt.Printf("%s is %d years old.\n", name, age)
+
+	fmt.Printf("%v is %d years old. \t and the type is %T and %T", name, age, name, age)
+
+}
---
+++ 02-formatPrinting/go.mod
@@ -0,0 +1,3 @@
+module formatPrinting
+
+go 1.22.4
