59ad4976b481201f9b708b7e3d0eaac07191ed57
sam <sam>
2024-07-10 11:06:57 -0600

    modified c2 client

--- 1337-blackhatgo/c2/client/client.go
+++ 1337-blackhatgo/c2/client/client.go
@@ -1,36 +1,47 @@
 package main
 
 import (
-	"c2/grpcapi"
 	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"c2/grpcapi"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 	var (
-		opts   []grpc.DialOption
 		conn   *grpc.ClientConn
 		err    error
 		client grpcapi.AdminClient
 	)
 
-	opts = append(opts, grpc.WithInsecure())
-	if conn, err = grpc.Dial(fmt.Sprintf("localhost:%d", 9090), opts...); err != nil {
+	// Set a timeout for the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Use insecure credentials for the connection
+	conn, err = grpc.DialContext(ctx, fmt.Sprintf("localhost:%d", 9090), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
 	client = grpcapi.NewAdminClient(conn)
+
 	var cmd = new(grpcapi.Command)
 	cmd.In = os.Args[1]
-	ctx := context.Background()
-	cmd, err = client.RunCommand(ctx, cmd)
+
+	// Create a new background context for the command execution
+	cmdCtx := context.Background()
+
+	cmd, err = client.RunCommand(cmdCtx, cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(cmd.Out)
-
 }
